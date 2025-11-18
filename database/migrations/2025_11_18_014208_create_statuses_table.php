baf758499539b6c980633a5543daefa269b84fde
mohamad shahril bin mohammad mahfud <www.sarelpirates@gmail.com>
2025-11-18 09:49:46 +0800

    create status table

---
+++ database/migrations/2025_11_18_014208_create_statuses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();   // e.g., Approved, Pending, Rejected
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+
+        // Insert default statuses
+        DB::table('statuses')->insert([
+            ['name' => 'Pending', 'description' => 'Waiting for approval'],
+            ['name' => 'Approved', 'description' => 'Request has been approved'],
+            ['name' => 'Rejected', 'description' => 'Request has been rejected'],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('statuses');
+    }
+};
